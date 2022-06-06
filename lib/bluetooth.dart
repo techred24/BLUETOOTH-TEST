c74b5b2b800cf05e4556dc223fb5658f7d3b289b
TeChRF24 <f.rafael.temich24@gmail.com>
2022-06-06 17:34:55 -0500

    bluetooth class done

---
+++ lib/bluetooth.dart
@@ -0,0 +1,103 @@
+import 'package:flutter_blue/flutter_blue.dart';
+import 'dart:convert';
+// import 'package:shared_preferences/shared_preferences.dart';
+
+class Indicador {
+
+  static final Indicador _indicador = Indicador._internal();
+  factory Indicador() {
+    return _indicador;
+  }
+  Indicador._internal();
+
+  BluetoothDevice dispositivoConectado;
+  // BluetoothDevice _dispositivo;
+  List<BluetoothDevice> dispositivos = [];
+  List<BluetoothDevice> disposivitosConectados = [];
+  List<BluetoothService> services = [];
+  String _identificador;
+  bool hayDispositivosConectados = false;
+  bool hayDispositivoConectado = false;
+  bool hayErrorEnConexion = false;
+
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+  _agregaDispositivoHaciaLista(final BluetoothDevice device) {
+    if (!dispositivos.contains(device)) {
+      print('DISPOSITIVO AGREGADO');
+      dispositivos.add(device);
+    }
+  }
+// TODO: Método para verificar si ya existen dispositivos conectados
+  void buscaDispositivosConectados() {
+    flutterBlue.connectedDevices
+      .asStream()
+      .listen((List<BluetoothDevice> dispositivos) {
+        for (BluetoothDevice dispositivo in dispositivos) {
+          // if (dispositivo.name == _identificador) {
+          if (dispositivo.name == 'Boletera-Apolo') {
+            _agregaDispositivoHaciaLista(dispositivo);
+          }
+        }
+      });
+      if (dispositivos.length > 0) hayDispositivosConectados = true;
+      hayDispositivosConectados = false;
+  }
+// TODO: Método que buscará y agregará dispositivo a la lista de dispositivos
+  void buscaDispositivos() {
+    flutterBlue.startScan(timeout: Duration(seconds: 3));
+    flutterBlue.scanResults.listen((results) async {
+      for (ScanResult result in results) {
+        if (result.device.name == 'Boletera-Apolo') {
+          _agregaDispositivoHaciaLista(result.device);
+        }
+      }
+    });
+  }
+  
+  // TODO:  Conectar dispotivo de la lista de dispositivos
+  void conectarDispositivo() async {
+    if (dispositivos.length > 0) {
+        for (final BluetoothDevice dispositivo in dispositivos) {
+          await dispositivo.connect();
+          // services = await dispositivo.discoverServices();
+          dispositivoConectado = dispositivo;
+          break;
+        }
+    }
+  }
+
+  void emitirSonido() async {
+    if (!hayErrorEnConexion) {
+      List<BluetoothDevice> connectedDevices = [];
+      // BluetoothDevice connectedOne;
+      flutterBlue.connectedDevices
+        .asStream()
+        .listen((List<BluetoothDevice> dispositivos) {
+            for (BluetoothDevice dispositivo in dispositivos) {
+                // if (dispositivo.name == _identificador) {
+              if (dispositivo.name == 'Boletera-Apolo') {
+                // _agregaDispositivoHaciaLista(dispositivo);
+                if (!connectedDevices.contains(dispositivo)) {
+                  connectedDevices.add(dispositivo);
+                }
+              }
+            }
+        });
+      
+      if (connectedDevices.length > 0) {
+        for (final BluetoothDevice device in connectedDevices) {
+          List<BluetoothService> services = await device.discoverServices();
+          for(final BluetoothService service in services) {
+            List<BluetoothCharacteristic> characteristics = service.characteristics;
+            for (final BluetoothCharacteristic characteristic in characteristics) {
+              if (characteristic.properties.write) {
+                characteristic.write(utf8.encode('1'));
+                break;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- lib/screens/fourth.dart
+++ lib/screens/fourth.dart
@@ -10,10 +10,45 @@ class Fourth extends StatelessWidget {
       ),
       body: Center(
         child: TextButton(
-          onPressed: () => Navigator.pushNamed(context, 'home'),
-          child: Text('Regresar a home')
+          onPressed: () => displayDialog(context),
+          child: Text('Abrir modal')
         )
       ),
     );
   }
+}
+
+void displayDialog(BuildContext context) {
+  showDialog(
+    barrierDismissible: false,
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadiusDirectional.circular(10)),
+        title: Text('Emisor de sonido'),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget> [
+            Text('Emitir Sonido'),
+            SizedBox(height: 10),
+          ]
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('Cancelar')
+          ),
+          TextButton(
+            onPressed: emitir(),
+            child: Text('Emitir')
+          )
+        ],
+      );
+    }
+  );
+}
+
+emitir() {
+  
 }
